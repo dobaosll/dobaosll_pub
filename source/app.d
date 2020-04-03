e1f4c0c57e6e0fa6043eb614f48042d0584ef064
Vladimir Shabunin <va.shabunin@physics.msu.ru>
2020-04-03 15:39:37 +0000

    hello, friend

---
+++ source/app.d
@@ -0,0 +1,139 @@
+import core.thread;
+import std.base64;
+import std.json;
+import std.functional;
+import std.stdio;
+
+import clid;
+import clid.validate;
+
+import baos_ll;
+import redis_dsm;
+import errors;
+
+enum CEMI_FROM_BAOS = "from_baos";
+enum CEMI_TO_BAOS = "to_baos";
+
+// struct for commandline params
+private struct Config {
+  @Parameter("config_prefix", 'c')
+    @Description("Prefix for config key names. dobaosll_config_uart_device, etc.. Default: dobaosll_config_")
+    string config_prefix;
+
+  @Parameter("device", 'd')
+    @Description("UART device. Setting this argument will overwrite redis key value. Default: /dev/ttyAMA0")
+    string device;
+}
+
+void main() {
+  writeln("hello, friend");
+
+  // baos global
+  BaosLL baos;
+
+  auto dsm = new RedisDsm("127.0.0.1", cast(ushort)6379);
+
+  auto config = parseArguments!Config();
+  string config_prefix = config.config_prefix.length > 1 ? config.config_prefix: "dobaosll_config_";
+
+  auto device = dsm.getKey(config_prefix ~ "uart_device", "/dev/ttyAMA0", true);
+  // if device parameter was given in commandline arguments
+  if (config.device.length > 1) {
+    device = config.device;
+    dsm.setKey(config_prefix ~ "uart_device", device);
+  }
+  auto params = dsm.getKey(config_prefix ~ "uart_params", "19200:8E1", true);
+
+  auto req_channel = dsm.getKey(config_prefix ~ "req_channel", "dobaosll_req", true);
+  auto cast_channel = dsm.getKey(config_prefix ~ "bcast_channel", "dobaosll_cast", true);
+  dsm.setChannels(req_channel, cast_channel);
+
+  auto stream_prefix = dsm.getKey(config_prefix ~ "stream_prefix", "dobaosll_stream_", true);
+  auto stream_maxlen = dsm.getKey(config_prefix ~ "stream_maxlen", "100000", true);
+
+  void handleRequest(JSONValue jreq, void delegate(JSONValue) sendResponse) {
+    JSONValue res;
+
+    auto jmethod = ("method" in jreq);
+    if (jmethod is null) {
+      res["success"] = false;
+      res["payload"] = Errors.no_method_field.message;
+      sendResponse(res);
+      return;
+    }
+    auto jpayload = ("payload" in jreq);
+    if (jpayload is null) {
+      res["success"] = false;
+      res["payload"] = Errors.no_payload_field.message;
+      sendResponse(res);
+      return;
+    }
+
+    string method = ("method" in jreq).str;
+    switch(method) {
+      case "cemi to bus":
+        try {
+          res["method"] = "success";
+          if (jreq["payload"].type() != JSONType.string) {
+            throw Errors.wrong_payload_type;
+          }
+          ubyte[] cemi;
+          try {
+            cemi = Base64.decode(jreq["payload"].str);
+          } catch (Base64Exception) {
+            throw Errors.wrong_base64_string;
+          }
+          writeln("sengind cemi, friend: ", cemi);
+          baos.sendFT12Frame(cemi);
+          res["payload"] = true;
+          sendResponse(res);
+
+          // add to redis stream
+          auto jstream = parseJSON("[]");
+          jstream.array ~= JSONValue(CEMI_TO_BAOS); // 1 - to bus
+          jstream.array ~= jreq["payload"];
+          dsm.addToStream(stream_prefix, stream_maxlen, jstream);
+        } catch(Exception e) {
+          res["method"] = "error";
+          res["payload"] = e.message;
+          sendResponse(res);
+        }
+        break;
+      default:
+        res["method"] = "error";
+        res["payload"] = Errors.unknown_method.message;
+        sendResponse(res);
+        break;
+    }
+  }
+  dsm.subscribe(toDelegate(&handleRequest));
+
+  baos = new BaosLL();
+  void onCemiFrame(ubyte[] cemi) {
+    writeln("cemi frame, friend");
+    auto jcast = parseJSON("{}");
+    jcast["method"] = "cemi from bus";
+    jcast["payload"] = Base64.encode(cemi);
+    dsm.broadcast(jcast);
+
+    // add to redis stream
+    auto jstream = parseJSON("[]");
+    jstream.array ~= JSONValue(CEMI_FROM_BAOS); // 1 - to bus
+    jstream.array ~= jcast["payload"];
+    dsm.addToStream(stream_prefix, stream_maxlen, jstream);
+  }
+  baos.onCemiFrame = toDelegate(&onCemiFrame);
+  writeln("BAOS instance created");
+  Thread.sleep(10.msecs);
+  baos.reset();
+  baos.switch2LL();
+  writeln("Switching to LinkLayer");
+  writeln("Working....");
+
+  while(true) {
+    dsm.processMessages();
+    baos.processIncomingData();
+    // check connections for timeout
+    Thread.sleep(1.msecs);
+  }
+}
---
+++ source/dsm.d
@@ -0,0 +1,118 @@
+//  Datapoint Sdk Messaging
+//  module provides with class to access 
+//  redis pub/sub layer for interprocess communication
+//  requests are sent to req_channel in stringified JSON
+//    response_channel: "client_listener",
+//    method: "get value",
+//    payload: 1/[1, 2, 3]/null
+//  }
+//  response then is sent to "client_listener"
+//  {
+//    method: "error/success",
+//    payload: {id: 1, value: 1, raw: "AZAz=" }/[{], ]/"string"
+//  }
+
+module redis_dsm;
+import std.conv;
+import std.stdio;
+import std.json;
+import std.functional;
+
+import tinyredis;
+import tinyredis.subscriber;
+
+class RedisDsm {
+  private Redis pub, redis;
+  private Subscriber sub;
+  private string redis_host, req_channel, bcast_channel;
+  private string redis_stream;
+  private ushort redis_port;
+
+  this(string redis_host, ushort redis_port, string req_channel,
+      string bcast_channel) {
+    // init publisher
+
+    this.redis_host = redis_host;
+    this.redis_port = redis_port;
+    this.req_channel = req_channel;
+    this.bcast_channel = bcast_channel;
+    redis = new Redis(redis_host, redis_port);
+  }
+  this(string redis_host, ushort redis_port) {
+    // init publisher
+
+    this.redis_host = redis_host;
+    this.redis_port = redis_port;
+    redis = new Redis(redis_host, redis_port);
+  }
+  public void setChannels(string req_channel, string bcast_channel) {
+    this.req_channel = req_channel;
+    this.bcast_channel = bcast_channel;
+  }
+
+  public void subscribe(void delegate(JSONValue, void delegate(JSONValue)) req_handler) {
+    // TODO: service_handler
+    // init publisher
+    pub = new Redis(redis_host, redis_port);
+    // delegate for incoming messages
+    void handleMessage(string channel, string message)
+    {
+      try {
+        JSONValue jreq = parseJSON(message);
+
+        // check if request is object
+        if (!jreq.type == JSONType.object) {
+          return;
+        }
+        // check if request has response_channel field
+        auto jresponse_channel = ("response_channel" in jreq);
+        if (jresponse_channel is null) {
+          return;
+        }
+        auto response_channel = ("response_channel" in jreq).str;
+
+        void sendResponse(JSONValue res) {
+          pub.send("PUBLISH", response_channel, res.toJSON());
+        }
+        req_handler(jreq, &sendResponse);
+      } catch(Exception e) {
+        //writeln("error parsing json: %s ", e.msg);
+      } 
+    }
+    sub = new Subscriber(redis_host, redis_port);
+    writeln("Subscribing to ", req_channel);
+    sub.subscribe(req_channel, toDelegate(&handleMessage));
+  }
+  public void broadcast(JSONValue data) {
+    pub.send("PUBLISH", bcast_channel, data.toJSON());
+  }
+  public void processMessages() {
+    sub.processMessages();
+  }
+  public string getKey(string key) {
+    return redis.send("GET " ~ key).toString();
+  }
+  public string getKey(string key, string default_value, bool set_if_null = false) {
+    auto keyValue = redis.send("GET " ~ key).toString();
+
+    if (keyValue.length > 0) {
+      return keyValue;
+    } else if (set_if_null) {
+      redis.send("SET " ~ key ~ " " ~ default_value);
+      return default_value;
+    } else {
+      return default_value;
+    }
+  }
+  public string setKey(string key, string value) {
+    return redis.send("SET " ~ key ~ " " ~ value).toString();
+  }
+  public void addToStream(string key_prefix, string maxlen, JSONValue data) {
+    auto command = "XADD ";
+    command ~= key_prefix ~ data.array[0].str ~ " ";
+    command ~= "MAXLEN ~ " ~ to!string(maxlen) ~ " ";
+    command ~= "* "; // id
+    command ~= "payload " ~ data.array[1].toJSON() ~ " ";
+    redis.send(command);
+  }
+}
